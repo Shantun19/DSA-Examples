b8a1ef0d7af6f35b0a343eac3dd3033dc4b43aeb
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2024-11-06 13:12:23 +0530

    Update 02_ReverseNumber.java

--- 02_ReverseNumber.java
+++ 02_ReverseNumber.java
@@ -20,3 +20,24 @@ class Solution {
         return (int)finalNum;
     }
 }
+
+// or 
+
+class Solution {
+    public int reverse(int n) {
+        return reversedDigits(n);
+    }
+
+    public static int reversedDigits(int n) { // 123
+        long result = 0;
+
+        while(n != 0) {
+            result = (result * 10) + (n % 10);
+            n/=10;
+        }
+        
+        if(result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) return 0;
+        if(n < 0) return (int)(-1 * result);
+        return (int)result;
+    }
+}
