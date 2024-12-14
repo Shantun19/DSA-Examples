853df01b242e30c59368a135b7f24e66d191ba89
Shantun Tomar <164908306+Shantun19@users.noreply.github.com>
2024-12-14 22:52:31 +0530

    Update 05_ReverseAnArray.java

--- 05_ReverseAnArray.java
+++ 05_ReverseAnArray.java
@@ -1,23 +1,76 @@
-import java.util.*;
-
-public class Sample {
+class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] arr = {1 , 2 , 3 , 4 , 5};
-        reverse_arr(arr , 0 , arr.length - 1);
-        for(int e : arr) System.out.print(e + " ");
+        int[] arr = {1,2,3,4,5,6,7};
+        reverseByBF(arr);
+        System.out.println();
+        reverseByApproach01(arr);
+        System.out.println();
+        reverseByApproach02(arr);
+        System.out.println();
+        reverseByApproach03(arr , 0 , arr.length-1);
+        for(int ele : arr) {
+            System.out.print(ele + " ");
+        }
     }
-
-    public static void reverse_arr(int[] arr , int start , int end) {
-       if(start >= end) return; // when start get bigger than end then the array gets reversed.
-
-        // swapping logic to swap the elements.
-       int t = arr[start];
-       arr[start] = arr[end];
-       arr[end] = t;
-
-       // recursive call.
-       reverse_arr(arr , start + 1 , end -1);
-
+    
+    // brute force approach.
+    // tc : o(n)
+    // sc : o(n)
+    public static void reverseByBF(int[] arr) {
+        int[] temp = new int[arr.length];
+        int index = 0;
+        for(int i=arr.length-1; i>=0; i--) {
+            temp[index++] = arr[i];
+        }
+        // print the array 
+        for(int ele : temp) {
+            System.out.print(ele + " ");
+        }
+    }
+    
+    // tc : o(n)
+    // sp : o(1)
+    public static void reverseByApproach01(int[] arr) {
+        int start_pointer = 0;
+        int end_pointer = arr.length - 1;
+        while(start_pointer <= end_pointer) {
+            int temp = arr[start_pointer];
+            arr[start_pointer] = arr[end_pointer];
+            arr[end_pointer] = temp;
+            start_pointer++;
+            end_pointer--;
+        }
+        // print the array 
+        for(int ele : arr) {
+            System.out.print(ele + " ");
+        }
+    }
+    
+    // tc : o(n)
+    // sp : o(1)
+    public static void reverseByApproach02(int[] arr) {
+        int n = arr.length;
+        for(int i=0; i<n/2; i++) {
+            int t = arr[i];
+            arr[i] = arr[n-i-1];
+            arr[n-i-1] = t;
+        }
+        // print the array 
+        for(int ele : arr) {
+            System.out.print(ele + " ");
+        }
+    }
+    
+    // tc : (n)
+    // sc : o(1)
+    public static void reverseByApproach03(int[] arr , int start , int end)
+    {
+        if(start <= end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            reverseByApproach03(arr , start + 1 , end -1);
+        }
+        else return;
     }
 }
